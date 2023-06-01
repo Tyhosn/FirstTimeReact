d84a1803b4ade83f4dae57e6e762aec35b50dca1
Ty Hosn <Abolsonm3@gmail.com>
2023-05-31 22:19:35 -0700

    New React

---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Note(){
+  return(
+    <div>
+<h1> This is an app in React</h1>
+<p> This is the start </p>
+
+    </div>
+
+  )
+}
+
+export default Note;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+
+//5. Create a Note.jsx component to show a <div> element with a
+//<h1> for a title and a <p> for the content.
+
+//1. Create a new React app.
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import App from './components/App'
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
